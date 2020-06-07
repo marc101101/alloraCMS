8a69e66bb854352cc405ab1b2b60a477541e36b8
Markus Gude <progamer101101@gmx.de>
2020-06-07 15:20:25 +0200

    update

---
+++ alloraApp/src/app/dataService.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
+export class DataService {
+    constructor(private http: HttpClient) { }
+
+    dataUrl = 'https://x6ce6j7dq6.execute-api.eu-central-1.amazonaws.com/dev/info';
+
+    getData(): Observable<any> {
+        return this.http.get(this.dataUrl);
+    }
+
+    postData(body): Observable<any> {
+        return this.http.post(this.dataUrl, body);
+    }
+
+}
