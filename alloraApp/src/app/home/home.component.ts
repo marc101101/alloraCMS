937d4a1d1638b39744f68d3ecd26d8b962ad3ad6
Markus Gude <progamer101101@gmx.de>
2020-06-18 22:39:45 +0200

    added ebay model

--- alloraApp/src/app/home/home.component.ts
+++ alloraApp/src/app/home/home.component.ts
@@ -4,55 +4,78 @@ import { InfoObject } from '../models/module';
 import { AuthService } from '../services/auth.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './home.component.html'
+  selector: 'app-root',
+  templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-    title = 'alloraApp';
-    data: InfoObject;
-    objectKeys = [];
+  title = 'alloraApp';
+  data: InfoObject;
+  objectKeys = [];
 
-    constructor(public dataService: DataService, public authService: AuthService) {
+  constructor(
+    public dataService: DataService,
+    public authService: AuthService
+  ) {}
 
+  ngOnInit() {
+    this.dataService.getData().subscribe((result) => {
+      this.data = result;
+      this.objectKeys = Object.keys(this.data.data);
+      this.objectKeys.splice(this.objectKeys.length - 1, 1);
+    });
+  }
+
+  addArticle() {
+    this.data.data.ebay.push({
+      title: '',
+      price: '',
+      status: '',
+      url: '',
+      img_url: '',
+    });
+  }
+
+  removeArticle(article) {
+    if (window.confirm('Do you really want to delete this article?!')) {
+      this.data.data.ebay.splice(article, 1);
     }
+  }
 
-    ngOnInit() {
-        this.dataService.getData().subscribe(result => {
-            this.data = result;
-            this.objectKeys = Object.keys(this.data.data);
-        });
-    }
+  getKeys(article) {
+    return Object.keys(article);
+  }
 
-    logout() {
-        this.authService.logout();
-        this.authService.navToLogin();
-    }
+  logout() {
+    this.authService.logout();
+    this.authService.navToLogin();
+  }
 
-    getRowNumber(element) {
-        let returnVal = 1;
-        if (element.length >= 130) {
-            returnVal = 2;
-        }
-        if (element.length >= 260) {
-            returnVal = 3;
-        }
-        if (element.length >= 390) {
-            returnVal = 4;
-        }
-        if (element.length >= 520) {
-            returnVal = 5;
-        }
-        return returnVal;
+  getRowNumber(element) {
+    let returnVal = 1;
+    if (element.length >= 130) {
+      returnVal = 2;
     }
+    if (element.length >= 260) {
+      returnVal = 3;
+    }
+    if (element.length >= 390) {
+      returnVal = 4;
+    }
+    if (element.length >= 520) {
+      returnVal = 5;
+    }
+    return returnVal;
+  }
 
-    publish() {
-        this.data.last_update = Date.now();
-        this.dataService.postData(this.data).subscribe((result) => {
-            alert(result.message);
-        },
-            (error) => {
-                alert(error.message)
-            }
-        );
-    }
+  publish() {
+    this.data.last_update = Date.now();
+    this.dataService.postData(this.data).subscribe(
+      (result) => {
+        alert(result.message);
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
+  }
 }
--- alloraApp/src/app/models/module.ts
+++ alloraApp/src/app/models/module.ts
@@ -1,18 +1,27 @@
-export interface Data {
-    home_headline: string;
-    home_subtitle: string;
-    spenden_up_title: string;
-    spenden_up_text: string;
-    spenden_up_sepa_iban: string;
-    spenden_up_sepa_bank: string;
-    spenden_sub_title: string;
+export class Data {
+  home_headline: string;
+  home_subtitle: string;
+  spenden_up_title: string;
+  spenden_up_text: string;
+  spenden_up_sepa_iban: string;
+  spenden_up_sepa_bank: string;
+  spenden_sub_title: string;
+  ebay: Array<EbayElement>;
 }
 
-export interface InfoObject {
-    name: string;
-    version: string;
-    description: string;
-    author: string;
-    data: Data;
-    last_update: number;
+export class EbayElement {
+  title: string;
+  price: string;
+  status: string;
+  url: string;
+  img_url: string;
+}
+
+export class InfoObject {
+  name: string;
+  version: string;
+  description: string;
+  author: string;
+  data: Data;
+  last_update: number;
 }
