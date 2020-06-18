e45c3467f5047c5e9ad821a0f62b81ecdae18740
Markus Gude <progamer101101@gmx.de>
2020-06-18 22:58:22 +0200

    update urls

--- alloraApp/src/app/services/dataService.ts
+++ alloraApp/src/app/services/dataService.ts
@@ -5,17 +5,20 @@ import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    dataUrl = 'https://x6ce6j7dq6.execute-api.eu-central-1.amazonaws.com/dev/info';
+  dataUrl =
+    'https://1mb9tesvgc.execute-api.eu-central-1.amazonaws.com/dev/info';
 
-    getData(): Observable<any> {
+  getData(): Observable<any> {
+    return this.http.get(this.dataUrl, {
+      headers: { 'x-api-key': localStorage.getItem('token') },
+    });
+  }
 
-        return this.http.get(this.dataUrl, { headers: { 'x-api-key': localStorage.getItem("token") } });
-    }
-
-    postData(body): Observable<any> {
-        return this.http.post(this.dataUrl, body, { headers: { 'x-api-key': localStorage.getItem("token") } });
-    }
-
+  postData(body): Observable<any> {
+    return this.http.post(this.dataUrl, body, {
+      headers: { 'x-api-key': localStorage.getItem('token') },
+    });
+  }
 }
